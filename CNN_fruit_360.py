8dd046f59cdaba75656689b39b62c9a811493e18
olan <olantoj@hotmail.com>
2018-05-09 19:14:08 +0700

    create and test model

--- CNN_fruit_360.py
+++ CNN_fruit_360.py
@@ -7,31 +7,113 @@ def plot_image(image, label):
       plt.imshow(image)
       plt.title(label)
       plt.show()
-
-fruit_images = []
+      
+dictionary = {}
+number = 0
+      
+def class_to_number(c):
+      n = []
+      for i in c:
+            n.append(dictionary[i])
+      return np.array(n)
+      
 fruit_labels = []
+
+train_x = []
+train_y = []
+
+valid_x = []
+valid_y = []
+
 count = 0
 loop_count = 0
 
-# for fruit_dir in glob.glob("data/Training/*"):
-#       fruit_label = fruit_dir.split("\\")[-1]
-#       print(fruit_label)
-#       for fruit_images_path in glob.glob(fruit_dir + "/*"):
-#             fruit_labels.append(fruit_label)
-#             image = mpimg.imread(fruit_images_path)
-#             fruit_images.append(image)
+for fruit_dir in glob.glob("data/Training/*"):
+      fruit_label = fruit_dir.split("\\")[-1]
+      fruit_labels.append(fruit_label)
+      if fruit_label not in dictionary:
+            dictionary[fruit_label] = number
+            dictionary[number] = fruit_label
+            number+=1
 
-# import numpy as np
+train_labels = fruit_labels[:10]
 
-# train_x = np.array(fruit_images)
-# train_y = np.array(fruit_labels)
+for label in train_labels:         
+      print(label)
+      for fruit_dir in glob.glob("data/Training/" + label + "/*"):
+            train_y.append(label)
+            image = mpimg.imread(fruit_dir)
+            train_x.append(image)
+
+for label in train_labels:         
+      print(label)
+      for fruit_dir in glob.glob("data/Validation/" + label + "/*"):
+            valid_y.append(label)
+            image = mpimg.imread(fruit_dir)
+            valid_x.append(image)
+
+import numpy as np
+from keras.utils import to_categorical
+
+train_x = np.array(train_x)
+train_y = train_y + fruit_labels
+train_y = np.array(train_y)
 # np.save('fruit_images', train_x)
 # np.save('fruit_labels', train_y)
 
-train_x = np.load('fruit_images.npy')
-train_y = np.load('fruit_labels.npy')
+# train_x = np.load('fruit_images.npy')
+# train_y = np.load('fruit_labels.npy')
+train_y = class_to_number(train_y)
+train_y = to_categorical(train_y)
+train_y = train_y[:-len(fruit_labels)]
+
+valid_x = np.array(valid_x)
+
+valid_y = valid_y + fruit_labels
+valid_y = np.array(valid_y)
+valid_y = class_to_number(valid_y)
+valid_y = to_categorical(valid_y)
+valid_y = valid_y[:-len(fruit_labels)]
+
 
 train_x = train_x.astype('float32')
 train_x = train_x / 255
 
+import keras 
+from keras.models import Sequential, Input, Model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.advanced_activations import LeakyReLU
 
+batch_size = 64
+epochs = 2
+num_classes = 60
+
+#Add network
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),activation='linear',input_shape=(100,100,3),padding='same'))
+model.add(LeakyReLU(alpha=0.0))
+model.add(MaxPooling2D((2, 2),padding='same'))
+model.add(Conv2D(64, (3, 3), activation='linear',padding='same'))
+model.add(LeakyReLU(alpha=0.0))
+model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+model.add(Conv2D(128, (3, 3), activation='linear',padding='same'))
+model.add(LeakyReLU(alpha=0.0))                  
+model.add(MaxPooling2D(pool_size=(2, 2),padding='same'))
+model.add(Flatten())
+model.add(Dense(128, activation='linear'))
+model.add(LeakyReLU(alpha=0.0))                  
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(),metrics=['accuracy'])
+
+model.summary()
+
+# from keras.models import load_model
+
+# model = load_model("fruit_model.h5py")
+
+#Train network
+model_train = model.fit(train_x, train_y, batch_size=batch_size,epochs=epochs,verbose=1,validation_data=(valid_x, valid_y))
+model.save("fruit_model.h5py")
---
+++ test_model.py
@@ -0,0 +1,52 @@
+from keras.models import load_model
+
+model = load_model("fruit_model.h5py")
+
+import glob
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+fruit_labels = []
+
+train_x = []
+train_y = []
+
+valid_x = []
+valid_y = []
+
+dictionary = {}
+number = 0
+
+import numpy as np
+      
+def class_to_number(c):
+      n = []
+      for i in c:
+            n.append(dictionary[i])
+      return np.array(n)
+
+for fruit_dir in glob.glob("data/Training/*"):
+      fruit_label = fruit_dir.split("\\")[-1]
+      fruit_labels.append(fruit_label)
+      if fruit_label not in dictionary:
+            dictionary[fruit_label] = number
+            dictionary[number] = fruit_label
+            number+=1
+
+train_labels = fruit_labels[:10]
+
+for label in train_labels:         
+      print(label)
+      for fruit_dir in glob.glob("data/Validation/" + label + "/*"):
+            valid_y.append(label)
+            image = mpimg.imread(fruit_dir)
+            valid_x.append(image)
+
+from numpy import argmax
+
+valid_x = np.array(valid_x)
+result = model.predict(valid_x[:10])
+result = argmax(result, axis=1)
+result = class_to_number(result)
+print('valid_y',valid_y[:10])
+print('result', result)
